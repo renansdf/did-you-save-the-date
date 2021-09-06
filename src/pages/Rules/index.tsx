a31ad428d0d97a49f8dd504b33d9fb500d93635a
Renan de Freitas <rnn.frts@gmail.com>
2021-09-06 11:52:20 -0300

    updating

--- src/pages/Rules/index.tsx
+++ src/pages/Rules/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container } from './styles';
 
 const Rules: React.FC = () => {
   return (
     <Container>
       <h1>Rules</h1>
+      <Link to="dashboard">Start game</Link>
     </Container>
   )
 };
